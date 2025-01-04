e895363fa0c202cc49e0a8cca025432ef5b19469
unknown <rdenblaauwen@gmail.com>
2025-01-04 17:01:20 +0100

    Moved AttractableObject to the  folder. Added some comments to the IDamageEvent and IDamageable that point towards examples of scripts that implement them.

--- Assets/Scripts/IDamageEvent.cs
+++ Assets/Scripts/IDamageEvent.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
 // Interface for classes which bundle data related to an event that attempts to damage something 
+// Objects with this interface are meant to be passed to objects that implement the IDamageable interface
+// `Script/CollisionDamageEvent` is an example of how this interface can be implemented
+// `Script/GERV/Testing/DealDamageOnCollision` is an example of a script that creates a damage event
 public interface IDamageEvent
 {
     // Incoming damage. This is not necessarily the damage that actually ends up being dealt,
--- Assets/Scripts/IDamageable.cs
+++ Assets/Scripts/IDamageable.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+// Interface which lets objects take damage
+// And example implementation of this interface can be found in `Script/HealthController`
 public interface IDamageable
 {
     public void Damage(IDamageEvent damageEvent);
